b8396654135cf7c42996ed59058d144e099761c1
MayuriXx <evanom59@gmail.com>
2020-08-30 16:35:57 +0200

    feat : add graphql with front
    graphql
    client 
    mutation
    app loading view
    home view

---
+++ lib/data/remote/countries/dto/country.dto.dart
@@ -0,0 +1,15 @@
+class CountryDto {
+  String name;
+
+  CountryDto({this.name});
+
+  CountryDto.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    return data;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:bus_rental_keolis/app.dart';
+import 'package:bus_rental_keolis/data/remote/countries/countries_client.dart';
 import 'package:bus_rental_keolis/view/home/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() async {
+
   WidgetsFlutterBinding.ensureInitialized();
 
   //Portrait mode
